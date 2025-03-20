ec3af5bc46c54ad5464467ac7b3fe47b6c5bf20e
deepencoding <deepanshugautam5111004@gmail.com>
2025-03-20 19:37:12 +0530

    Added vercel speed insights.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	function goTop() {
 		document.body.scrollIntoView();
 	}
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 </script>
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
 	<div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (y>0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0') }>
@@ -23,3 +24,4 @@
 </div>
 
 <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth></svelte:window>
+injectSpeedInsights();
